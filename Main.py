383f869ba049233066c5c3a980a5370cdf6dc30a
Safwan Ahmed <safwan20853@gmail.com>
2024-03-04 22:04:56 -0500

    first commit

--- Main.py
+++ Main.py
@@ -3,8 +3,16 @@ username = input("Enter username: ")
 password = input("Enter password: ")
 # for right now this only works for mcps
 sv = StudentVue(username, password, "https://md-mcps-psv.edupoint.com")
-gradebook = sv.get_gradebook()
-for i in gradebook["Gradebook"]["Courses"]["Course"]:
-    print(f"{i["@Title"]} taught by Professor {i["@Staff"]}")
-    
+firstQuarter = sv.get_gradebook(1)
+secondQuarter = sv.get_gradebook(3)
+thirdQuarter = sv.get_gradebook(5)
+fourthQuarter = sv.get_gradebook(7)
+
+print("FIRST SEMESTER")
+for i in firstQuarter["Gradebook"]["Courses"]["Course"]:
+    print(f"{i["@Title"]} taught by Professor {i["@Staff"]} ({i["@StaffEMail"]})")
+print("----------------------------------------")
+print("SECOND SEMESTER")
+for i in secondQuarter["Gradebook"]["Courses"]["Course"]:
+    print(f"{i["@Title"]} taught by Professor {i["@Staff"]} ({i["@StaffEMail"]})")
 
