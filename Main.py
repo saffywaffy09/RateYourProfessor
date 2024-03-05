c9715efdecfa4c78600db6cf11679aa3ad7fa994
Andrei D <adorobantu2008@gmail.com>
2024-03-05 08:05:29 -0500

    Incognito Password

--- Main.py
+++ Main.py
@@ -1,9 +1,27 @@
 from studentvue import StudentVue
+from getpass import getpass
+
+
 username = input("Enter username: ")
-password = input("Enter password: ")
+password = getpass()
+
+
 # for right now this only works for mcps
 sv = StudentVue(username, password, "https://md-mcps-psv.edupoint.com")
 gradebook = sv.get_gradebook()
 
-for i in gradebook["Gradebook"]["Courses"]["Course"]:
-    print(i["@Title"], "taught by", i["@Staff"])
+
+
+schedule = sv.get_schedule(1)
+if "RT_ERROR" in schedule.keys():
+    print("bad credentials")
+studentClassSchedule = schedule['StudentClassSchedule']
+classLists = studentClassSchedule['ClassLists']
+classListing = classLists['ClassListing']
+
+for i in classListing:
+    print(i["@CourseTitle"])
+
+print()
+
+
