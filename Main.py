b2bfc5d05ab9542a730de4566640efc521a973c0
Safwan Ahmed <safwan20853@gmail.com>
2024-03-04 20:52:54 -0500

    first commit

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from studentvue import StudentVue
+sv = StudentVue("172834", "not my real password", "https://md-mcps-psv.edupoint.com")
 gradebook = sv.get_gradebook()
 print(gradebook["Gradebook"]["Courses"]["Course"])
 for i in gradebook["Gradebook"]["Courses"]["Course"]:
