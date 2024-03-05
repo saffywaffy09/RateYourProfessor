5db4db10505f38d72c31e48b3c07fdec9f6bc08d
Andrei D <adorobantu2008@gmail.com>
2024-03-04 21:37:51 -0500

    Formatting fix

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
 from studentvue import StudentVue
-sv = StudentVue("172834", "not my real password", "https://md-mcps-psv.edupoint.com")
+username = input("Enter username: ")
+password = input("Enter password: ")
+# for right now this only works for mcps
+sv = StudentVue(username, password, "https://md-mcps-psv.edupoint.com")
 gradebook = sv.get_gradebook()
-print(gradebook["Gradebook"]["Courses"]["Course"])
-for i in gradebook["Gradebook"]["Courses"]["Course"]:
-    print(i["@Title"])
 
+for i in gradebook["Gradebook"]["Courses"]["Course"]:
+    print(i["@Title"], "taught by", i["@Staff"])
