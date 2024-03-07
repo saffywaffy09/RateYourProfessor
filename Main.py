c9a2055b8e3d84b712ad3ad1411ee8fd7b8b4fd2
Andrei D <adorobantu2008@gmail.com>
2024-03-07 08:32:03 -0500

    added classInfo usage

--- Main.py
+++ Main.py
@@ -30,6 +30,20 @@ def addSchedule():
             [(index + 1), i["@Title"], i["@Staff"], i["@StaffEMail"], i["Marks"]["Mark"]["@CalculatedScoreRaw"]])
         instancesOfTeacher = cursor.execute("SELECT teacherName FROM teacherInfo WHERE teacherName = ?",
                                             (info[index][2],)).fetchall()
+        instanceOfClass = cursor.execute("SELECT className FROM classInfo WHERE className = ?", (info[index][1], )).fetchall()
+
+        if len(instanceOfClass) == 0:
+            cursor.execute("INSERT INTO classInfo VALUES (?, ?, ?, ?, ?)",
+                           (info[index][1], info[index][2], -1, str([]), str([])))
+        else:
+            currTeacher = info[index][2]
+            if not currTeacher in toArr(cursor.execute("SELECT classTeachers FROM classInfo WHERE className = ?", (info[index][1], )).fetchone()[0]):
+                arr = toArr(cursor.execute("SELECT classTeachers FROM classInfo WHERE className = ?", (info[index][1], )).fetchone()[0])
+                arr.append(currTeacher)
+                cursor.execute("UPDATE classInfo SET classTeachers = ? WHERE className = ?", (str(arr), info[index][1]))
+
+
+
         if len(instancesOfTeacher) == 0:
             cursor.execute("INSERT INTO teacherInfo VALUES (?, ?, ?, ?, ?, ?)",
                            (info[index][2], info[index][3], -1 , str([info[index][1]]), str([]), str([])))
@@ -41,13 +55,13 @@ def addSchedule():
                 arr.append(currClass)
                 cursor.execute("UPDATE teacherInfo SET teacherClasses = ? WHERE teacherName = ?", (str(arr), i["@Staff"]))
 
-            pass
 
     if not addedData:
         for index, i in enumerate(info):
             cursor.execute("INSERT INTO allInfo VALUES \n\t" + toString(i))
         connection.commit()
 
+    connection.commit()
 
 # ****************************************************MAIN**********************************************************
 
