571a60f336d3701e1aa59525fe428fe1d919f407
Andrei D <adorobantu2008@gmail.com>
2024-03-15 12:47:31 -0400

    had an unnecessary function

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ app.config["DATABASE"] = 'example.db'
 
 # Define your routes
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(app.config['DATABASE'], check_same_thread=False)
-    return db
-
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
